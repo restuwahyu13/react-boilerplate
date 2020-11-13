fdc8699b74385c8758e6e1703c5a7e6dc2bf3cae
restuwahyu13 <restuwahyu13@gmail.com>
2020-11-13 22:55:31 +0700

    update config and add new package

--- src/index.js
+++ src/index.js
@@ -17,5 +17,5 @@ ReactDOM.render(<App />, document.getElementById('root'))
 @description on serviceWorker if environment is production mode
 */
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in window.navigator) {
-  window.onload = () => window.navigator.serviceWorker.register(`/service-worker.js`)
+  window.onload = () => window.navigator.serviceWorker.register('/service-worker.js')
 }
