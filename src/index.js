34a704105008233bafe66cd2acbcc5d3e4894c34
restuwahyu13 <restuwahyu13@gmail.com>
2020-11-21 14:16:31 +0700

    change styled css to scss and update config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './index.css'
+import './index.scss'
 
 /**
 @description render app component
@@ -12,5 +12,5 @@ ReactDOM.render(<App />, document.getElementById('root'))
 @description on serviceWorker if environment is production mode
 */
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in window.navigator) {
-	window.onload = () => window.navigator.serviceWorker.register('/service-worker.js')
+  window.onload = () => window.navigator.serviceWorker.register('/service-worker.js')
 }
