cd24a63a3bb2157742e746a1a684d9abf3d1c804
restuwahyu13 <restuwahyu13@gmail.com>
2020-11-21 14:02:26 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
-import React from 'react'
 import App from './App'
 
 test('react match text paragraph value', () => {
   const wrapper = shallow(<App />)
-  expect(wrapper.find('.App-link').text()).toEqual('Learn React')
+  expect(wrapper.find('code').first().text()).toEqual('Minimalize Modern React Boilerplate 2020')
 })
